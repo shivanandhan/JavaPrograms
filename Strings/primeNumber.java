8e7d77f8c4e782d91de79a98f897afc7212b285c
Shiva <shivanandhan33@gmail.com>
2024-11-13 12:16:32 +0530

    strings

--- Strings/primeNumber.java
+++ Strings/primeNumber.java
@@ -4,10 +4,30 @@ public class primeNumber {
 
     public static void main(String[] args)
     {
-        int num=10;
-
+        int num=7;
+        boolean flag = false;
+        //0 and 1 are not prime numbers
         if(num ==0||num==1)
         {
+            flag =true;
+        }
+        for(int i =2; i<=num/2;i++)
+        {
+            //condition for non-prime
+            if(num%i==0)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+
+        {
+            System.out.println(num + " : " + "primeNumber");
+        }
+
+        else {
+            System.out.println(num + " : " + "not a primeNumber");
         }
     }
 }
